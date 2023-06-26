f9b77dafa0fdcb465c1c238d73d7e3d913358d6c
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-06-26 16:54:16 +0600

    up

--- js/main.js
+++ js/main.js
@@ -2,25 +2,42 @@ $(window).load(function () {
   var c = document.getElementById('canvas');
   c.style.background = "#c51d68e3";
   let going = "down"
+  var ctx2 = c.getContext('2d');
   var ctx = c.getContext('2d');
+
   var RADIUS = 300;
   var num_sections = 8; //set this for number of divisions
-  function drawCircle(x, y, r, pos) {
-    ctx.beginPath();
-    ctx.strokeStyle = '#fff';
+
+  function drawCircle(x, y, r, pos = 1.5, style = '#fff', ctxInfo=null) {
+    if (ctxInfo) {
+      ctxInfo.beginPath();
+      ctxInfo.strokeStyle = style
+      console.log(pos);
+      ctxInfo.arc(x, y, r, 1.5 * Math.PI, pos * Math.PI);
+      ctxInfo.stroke();
+    
+      return false
+    } else {
+       ctx.beginPath();
+    ctx.strokeStyle = style
+    console.log(pos);
     ctx.arc(x, y, r, 1.5 * Math.PI, pos * Math.PI);
     ctx.stroke();
+    }
+   
   }
+
+  drawCircle(380, 380, RADIUS,1.4999,'#000',ctx2);
   function drawAngledLine(x, y, length, angle) {
     var radians = (angle / 180) * Math.PI;
     var endX = x + length * Math.cos(radians);
     var endY = y - length * Math.sin(radians);
     ctx.beginPath();
-    ctx.strokeStyle = '#fff';
+    // ctx.strokeStyle = '#fff';
     ctx.moveTo(x, y);
     ctx.lineTo(endX, endY); ctx.closePath();
 
-    ctx.stroke();
+    // ctx.stroke();
   }
   //draw circle outline
 
@@ -48,12 +65,18 @@ $(window).load(function () {
     let totalHeight = contentBox.scrollHeight;
 
     // Clear the canvas
-    ctx.clearRect(0, 0, c.width, c.height);
 
+    let pos = (scrollPos * 2) / totalHeight;
+    if (((scrollPos * 2) / totalHeight) <= 1.5) {
+       ctx.clearRect(0, 0, c.width, c.height);
+    }
     if (scrollPos > 0) {
       // Draw circle
       let circleY = scrollPos + (c.height / 2);
-      drawCircle(380, 380, RADIUS, ((scrollPos * 2) / totalHeight) - 0.05293831957260806);
+      if (((scrollPos * 2) / totalHeight) <= 1.5) {
+        drawCircle(380, 380, RADIUS, pos);
+      }
+    
     }
   });
 
